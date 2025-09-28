6f7f7ba83bb4a108d20d4430be872be01d048be7
Natali <natasadevat@gmail.com>
2025-09-28 19:32:27 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeImage() {
+            document.getElementById("image").src=
+            "https://cs14.pikabu.ru/post_img/2023/02/10/6/1676017190178416873.webp";
+            document.getElementById("image").alt="Котик 2";
+        }
